1fc94e460eb5558aab664db201439f4fd5e3eef6
Alberto Cardenas <alcardm@gmail.com>
2022-11-21 09:33:41 -0500

    Proyecto frontend agregado.

---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,101 @@
+import React from "react";
+import axios from "axios";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import "./login.css";
+import { isNull } from "util";
+import Cookies from "universal-cookie";
+import { calculaExtraccionSesion } from "../helper/helper";
+import app from "../app.json";
+import Loading from "../loading/loading";
+
+const cookies = new Cookies();
+const { APIHOST } = app;
+
+export default class login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      usuario: "",
+      pass: "",
+    };
+  }
+  iniciarSesion() {
+    this.setState({ loading: true });
+    axios
+      .post(`${APIHOST}/usuarios/login`, {
+        usuario: this.state.usuario,
+        pass: this.state.pass,
+      })
+
+      .then((response) => {
+        if (isNull(response.data.token)) {
+          alert("usuario y/o contraseña invalidas");
+        } else {
+          cookies.set("_s", response.data.token, {
+            Path: "/",
+            expires: calculaExtraccionSesion(),
+          });
+          this.props.history.push(window.open('/empleados'));
+        }
+        this.setState({ loading: false });
+      })
+
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <Container id="login-container">
+        <Loading show={this.state.loading} />
+        <Row>
+          <Col>
+            <Row>
+              <h2> Renta de Lavadoras </h2>
+            </Row>
+            <Row>
+              <Col
+                sm="12"
+                xs="12"
+                md={{ span: 4, offset: 4 }}
+                lg={{ span: 4, offset: 4 }}
+                xl={{ span: 4, offset: 4 }}
+              >
+                <Form>
+                  <Form.Group>
+                    <Form.Label> Usuario </Form.Label>
+                    <Form.Control
+                      onChange={(e) =>
+                        this.setState({ usuario: e.target.value })
+                      }
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label> Contraseña </Form.Label>
+                    <Form.Control
+                      type="password"
+                      onChange={(e) => this.setState({ pass: e.target.value })}
+                    />
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      this.iniciarSesion();
+                    }}
+                  >
+                    Iniciar Sesion
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
---
+++ frontend/src/components/router/router.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+import Login from '../login/login';
+import Inicio from '../index/index';
+import PrivateRouter from '../auth/privaterouter';
+import Empleados from '../empleados/empleados.buscar';
+
+export default function AppRoutes(){
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={["/login"] } component={ Login } />
+                <PrivateRouter exact path={ ["/empleados"] } component={ Empleados } />
+                <Route exact path={ ["/"] } component={ Inicio } />
+                <Route exact path={ ["/index"] } component={ Inicio } />
+                <Route path={"*"} component={() => (
+                    <h1 style={{marginTop: 300}}> 
+                        404
+                        <br />
+                        Pagina No Encontrada
+                    </h1>
+                    )} />
+            </Switch>
+        </Router>
+    );
+}

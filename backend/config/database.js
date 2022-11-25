abf3b713cead68d05b41d534f004cee9a2071bec
Alberto Cardenas <alcardm@gmail.com>
2022-11-25 18:14:32 -0500

    Configurando backend para despliegue en Vercel.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-const host = "localhost";
+const host = "mongodb://localhost";
 const port = "27017";
 const db = "hr";
 
 exports.mongoConnect = () => {
-    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
-    mongoose.connect(mongoStringConnection);
-    mongoose.Promise = global.Promise;
-    const dbConnection = mongoose.connection;
-    dbConnection.on("error", console.error.bind(console,"Mongodb connection error"))
-}
+  // const mongoStringConnection = "" + host + ":" + port + "/" + db;
+  const mongoStringConnection =
+    "mongodb+srv://alcardm:G5i9DvBvYQ65i0Ck@cluster0.p4r4ura.mongodb.net/" + db;
+  mongoose.connect(mongoStringConnection);
+  mongoose.Promise = global.Promise;
+  const dbConnection = mongoose.connection;
+  dbConnection.on(
+    "error",
+    console.error.bind(console, "Mongodb connection error")
+  );
+};
